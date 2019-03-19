7d8b20336e85f5e5a75629000cc542a5bc3ed3fd
Dawid Barsznica <dbarsznica@gmail.com>
2019-03-19 15:37:53 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var fs = require('fs');
-var folder = "../17.7-File_System/"; 
-fs.readdir(folder, function(err, files) {
+var fs = require('fs'); 
+fs.readdir('./', function(err, files) {
     if (err) throw err;
-    var files = files;
     console.log(files)
     fs.writeFile('files.txt', files, function(err) {
         if (err) throw err;
